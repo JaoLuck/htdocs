5599fdde67469abe85456287e895c35952777be7
JãoLuck <jlsramires8@gmail.com>
2022-06-10 21:29:32 -0300

    aula 10.06

---
+++ contatos.php
@@ -0,0 +1,22 @@
+<?php
+	/*echo $_GET["nome"]; 
+	echo "<br />";
+	echo $_GET["email"];
+	echo "<br />";
+	echo $_GET["tipo-contato"];
+	echo "<br />";
+	echo $_GET["numero1"] + $_GET["numero2"];*/
+	$nome = $_GET["nome"];
+	$email = $_GET["email"];
+	$contato = $_GET["tipo-contato"];
+	$numero1 = $_GET["numero1"];
+	$numero2 = $_GET["numero2"];
+	echo $nome;
+	echo "<br />";
+	echo $email;
+	echo "<br />";
+	echo $contato;
+	echo "<br />";
+	echo $numero1;
+	echo "<br />";
+	echo $numero2;
